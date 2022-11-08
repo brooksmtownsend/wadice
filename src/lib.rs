5d2843903a7a28bba5601bf21a8e6ca36bfb115a
Brooks Townsend <brooks@cosmonic.com>
2022-11-08 15:41:11 -0500

    changed roll parsing from query to path
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-use d20::DieRollTerm;
-use rust_embed::RustEmbed;
 use wasmbus_rpc::actor::prelude::*;
 use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
+use wasmcloud_interface_logging::info;
 use wasmcloud_interface_numbergen::random_in_range;
 
-#[derive(RustEmbed)]
+/// Embedding favicon.ico for optimal tab experience
+#[derive(rust_embed::RustEmbed)]
 #[folder = "static/"]
 struct Asset;
 
@@ -15,14 +15,7 @@ struct WadiceActor {}
 /// Implementation of HttpServer trait methods
 #[async_trait]
 impl HttpServer for WadiceActor {
-    /// Returns a greeting, "Hello World", in the response body.
-    /// If the request contains a query parameter 'name=NAME', the
-    /// response is changed to "Hello NAME"
-    async fn handle_request(
-        &self,
-        _ctx: &Context,
-        req: &HttpRequest,
-    ) -> std::result::Result<HttpResponse, RpcError> {
+    async fn handle_request(&self, _ctx: &Context, req: &HttpRequest) -> RpcResult<HttpResponse> {
         if req.path.contains("favicon.ico") {
             if let Some(favicon) = Asset::get("favicon.ico") {
                 return Ok(HttpResponse {
@@ -31,10 +24,13 @@ impl HttpServer for WadiceActor {
                 });
             }
         }
-        let roll = form_urlencoded::parse(req.query_string.as_bytes())
-            .find(|(n, _)| n == "roll")
-            .map(|(_, v)| v.to_string())
-            .unwrap_or_else(|| "1d20".to_string());
+
+        let path = req.path.trim_matches('/');
+        let roll = if path.len() <= 0 {
+            "1d20".to_string()
+        } else {
+            path.to_string()
+        };
 
         let die_rolls = d20::parse_die_roll_terms(&roll);
 
@@ -42,11 +38,11 @@ impl HttpServer for WadiceActor {
         for roll in die_rolls {
             total = total
                 + match roll {
-                    DieRollTerm::Modifier(n) => n,
-                    DieRollTerm::DieRoll {
+                    d20::DieRollTerm::Modifier(n) => n,
+                    d20::DieRollTerm::DieRoll {
                         multiplier: m,
                         sides: s,
-                    } => (random_in_range(1, s as u32).await? as i8 * m),
+                    } => random_in_range(1, s as u32).await? as i8 * m,
                 }
         }
 
@@ -64,7 +60,7 @@ impl HttpServer for WadiceActor {
             total, total
         );
 
-        wasmcloud_interface_logging::info!("A user rolled a {}!", total);
+        info!("A user rolled a {}!", total);
 
         Ok(HttpResponse {
             body: resp_body.into_bytes(),
